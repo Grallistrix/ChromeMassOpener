79de224f5cd3e02b9db56bf3055dd712324af0f3
Grallistrix <czarne8@gmail.com>
2023-11-30 15:37:06 +0100

    filePath changed

---
+++ ChromeMassOpener/ChromeMassOpener.cpp
@@ -0,0 +1,43 @@
+﻿// SecretSecretProject.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <cstdio>
+#include <ctime>
+
+using namespace std;
+
+#define LIMIT 3
+int main()
+{
+
+    srand(time(NULL));
+    const string filePath = "";
+
+    fstream file;
+    file.open(filePath, std::ios::in);
+
+    if (file.is_open())
+        std::cout << "IsOpened\n";
+    else {
+        std::cout << "ERR_NotOpened\n";
+        return -9;
+    }
+
+    string line;
+    string chromeCommand = "start chrome ";
+
+    for (string line; getline(file, line);) {
+        line.erase(std::remove(line.begin(), line.end(), ' '), line.end());
+
+        string command = chromeCommand + line;
+        const char* com = command.c_str();
+        system(com);
+    }
+
+    file.close();
+}
