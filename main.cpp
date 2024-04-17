5f3d9d412ffc8f34baa951c97525892e9d619445
Andrzej Piotrowski <116823604+Grallistrix@users.noreply.github.com>
2024-04-17 02:17:38 +0200

    End of Cleanup

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    const string filePath = "PUT_YOUR_FILE_PATH_HERE";
+
+    fstream file;
+    file.open(filePath, std::ios::in);
+
+    if (file.is_open())
+        std::cout << "IsOpened\n";
+    else {
+        std::cout << "ERR_NotOpened\n";
+        return -9;
+    }
+
+    string line;
+    string chromeCommand = "start chrome ";
+
+    for (string line; getline(file, line);) {
+        line.erase(std::remove(line.begin(), line.end(), ' '), line.end());
+
+        string command = chromeCommand + line;
+        const char* com = command.c_str();
+        system(com);
+    }
+
+    file.close();
+}
